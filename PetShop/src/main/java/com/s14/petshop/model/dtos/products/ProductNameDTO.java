e5f755b030ef8a674da10a06be7d547179b71285
Yuzden <yuzden.4729@gmail.com>
2022-10-21 02:39:28 +0300

    Add ProductNameDTO

---
+++ PetShop/src/main/java/com/s14/petshop/model/dtos/products/ProductNameDTO.java
@@ -0,0 +1,12 @@
+package com.s14.petshop.model.dtos.products;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class ProductNameDTO {
+    private String name;
+}
