f687585d53db8e0a2c7af5f7107368ff36e68696
Yuzden <yuzden.4729@gmail.com>
2022-10-21 02:13:30 +0300

    Add methods for delete product, search product and add discount to product

--- PetShop/src/main/java/com/s14/petshop/controller/ProductController.java
+++ PetShop/src/main/java/com/s14/petshop/controller/ProductController.java
@@ -1,7 +1,7 @@
 package com.s14.petshop.controller;
 
-import com.s14.petshop.model.beans.Product;
-import com.s14.petshop.model.dtos.ProductDTO;
+import com.s14.petshop.model.dtos.products.ProductDTO;
+import com.s14.petshop.model.dtos.products.ProductNameDTO;
 import com.s14.petshop.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -21,17 +21,31 @@ public class ProductController extends AbstractController {
     }
 
     @DeleteMapping("/products/{pid}")
-    public void deleteProduct(@PathVariable int pid) {
+    public boolean deleteProduct(@PathVariable int pid) {
         //todo validate login
         //todo validate admin
-
+        return productService.deleteProduct(pid);
     }
 
     @PostMapping("/products")
     public ProductDTO addProduct(@RequestBody ProductDTO dto) {
         //todo validate login
         //todo validate admin
-
         return productService.addProduct(dto);
     }
+
+    @GetMapping("/product/search")
+    public ProductDTO searchProductByName(@RequestBody ProductNameDTO dto) {
+        ProductDTO result = productService.searchWithName(dto);
+        return result;
+    }
+
+    @PostMapping("/product/{pid}/discount/{did}")
+    public ProductDTO addDiscountToProduct(@PathVariable int pid, @PathVariable int did) {
+        //todo validate login and admin
+
+        ProductDTO productDTO = productService.addDiscountToProduct(pid,did);
+        return productDTO;
+    }
+
 }
--- PetShop/src/main/java/com/s14/petshop/service/ProductService.java
+++ PetShop/src/main/java/com/s14/petshop/service/ProductService.java
@@ -1,28 +1,34 @@
 package com.s14.petshop.service;
 
 import com.s14.petshop.model.beans.Product;
-import com.s14.petshop.model.dtos.ProductDTO;
+import com.s14.petshop.model.dtos.products.ProductDTO;
+import com.s14.petshop.model.dtos.products.ProductNameDTO;
+import com.s14.petshop.model.exceptions.BadRequestException;
 import com.s14.petshop.model.exceptions.NotFoundException;
 import com.s14.petshop.model.repositories.ProductRepository;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Service
-public class ProductService {
+public class ProductService extends AbstractService {
 
     @Autowired
     private ProductRepository productRepository;
 
     @Autowired
     private ModelMapper modelMapper;
+    @Autowired
+    private DiscountService discountService;
 
     public ProductDTO getById(int pid) {
         Product product = productRepository.findById(pid).orElseThrow(() -> new NotFoundException("Product not found!"));
         ProductDTO dto = modelMapper.map(product, ProductDTO.class);
         return dto;
     }
-
 
     public ProductDTO addProduct(ProductDTO dto) {
         if (productRepository.existsByName(dto.getName())) {
@@ -33,6 +39,40 @@ public class ProductService {
         product.setQuantity(1);
 
         productRepository.save(product);
+        return dto;
+    }
+
+    public boolean deleteProduct(int pid) {
+        if(!productRepository.existsById(pid)){
+            throw new BadRequestException("This product does not exist!");
+        }
+        Product product = productRepository.findById(pid).get();
+        product.setQuantity(product.getQuantity() - 1);
+        productRepository.save(product);
+        return true;
+    }
+
+    public ProductDTO searchWithName(ProductNameDTO dto) {
+        if(dto.getName() == null || dto.getName().isEmpty()){
+            throw new BadRequestException("Enter name for searching");
+        }
+
+        Product product = productRepository.findByName(dto.getName());
+        ProductDTO dtoResult = modelMapper.map(product, ProductDTO.class);
+        return dtoResult;
+    }
+
+    public ProductDTO addDiscountToProduct(int pid, int did) {
+        if(!productRepository.existsById(pid)){
+            throw new BadRequestException("The product does not exist");
+        }
+
+        Product product = productRepository.findById(pid).get();
+
+        product.setDiscount(discountService.getById(did));
+        productRepository.save(product);
+
+        ProductDTO dto = modelMapper.map(product, ProductDTO.class);
         return dto;
     }
 }
